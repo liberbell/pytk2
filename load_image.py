42dd26a27e9dfccb9e4c39a35680dc7948d4d06a
liberbell <liberbell@gmail.com>
2019-01-19 18:20:40 +0900

    load_image5

--- load_image.py
+++ load_image.py
@@ -6,4 +6,10 @@ from pathlib import Path
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-x_train = x_train.astype
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train / 255
+x_test = x_test /255
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
