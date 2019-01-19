2010b2cbc9241be4dd2204131cf2330bf0a97cb6
liberbell <liberbell@gmail.com>
2019-01-19 18:32:03 +0900

    dense_layers2

--- dense_layers.py
+++ dense_layers.py
@@ -13,3 +13,9 @@ x_test = x_test /255
 
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape(32, 32, 3)))
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
