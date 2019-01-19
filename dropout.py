5292d36534530ec8097fce9d6bd1aa81be1d290e
liberbell <liberbell@gmail.com>
2019-01-19 18:51:28 +0900

    dropout2

--- dropout.py
+++ dropout.py
@@ -18,10 +18,12 @@ model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
 
