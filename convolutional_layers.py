da80ed1e8e4195853636718f31e91f40e148ac7c
liberbell <liberbell@gmail.com>
2019-01-19 18:40:25 +0900

    convolutional_layers5

--- convolutional_layers.py
+++ convolutional_layers.py
@@ -16,6 +16,10 @@ y_test = keras.utils.to_categorical(y_test, 10)
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), )
 model.add(Dense(512, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
