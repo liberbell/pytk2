58303db9462f1f6c60248320bfdc6c7ccda137bd
liberbell <liberbell@gmail.com>
2019-01-19 18:48:37 +0900

    max_pooling2

--- max_pooling.py
+++ max_pooling.py
@@ -17,8 +17,12 @@ y_test = keras.utils.to_categorical(y_test, 10)
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
 model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 
 model.add(Dense(512, activation='relu'))
