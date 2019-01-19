1ee5424a76f0e7e3ee6885ba289258ddbeead42e
liberbell <liberbell@gmail.com>
2019-01-19 18:17:43 +0900

    load_image4

--- load_image.py
+++ load_image.py
@@ -2,5 +2,8 @@ import keras
 from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from pathlib import Path
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+x_train = x_train.astype
