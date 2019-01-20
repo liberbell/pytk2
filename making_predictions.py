f44612f143be65f55bf102dd17dcfc0ba7bd25dd
liberbell <liberbell@gmail.com>
2019-01-21 07:40:59 +0900

    making_predictions1

--- making_predictions.py
+++ making_predictions.py
@@ -20,4 +20,7 @@ f = Path('model_structure.json')
 model_structure= f.read_text()
 
 model = model_from_json(model_structure)
-model
+model.load_weight('model_weights.h5')
+
+img = image.load_img('cat.png', target_size=(32, 32))
+image_to_test = image.image_to_array(img)
