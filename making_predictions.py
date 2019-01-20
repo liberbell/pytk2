b4a9e3cc80a4fa8859fd4f00390313637bb995b3
liberbell <liberbell@gmail.com>
2019-01-21 07:43:40 +0900

    making_predictions3

--- making_predictions.py
+++ making_predictions.py
@@ -26,3 +26,6 @@ img = image.load_img('cat.png', target_size=(32, 32))
 image_to_test = image.image_to_array(img)
 
 list_of_images = np.expand_dims(image_to_test, axis=0)
+
+results = model.predict(list_of_images)
+single_result = results[0]
