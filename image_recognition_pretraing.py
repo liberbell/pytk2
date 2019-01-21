e52520cf14b9c674df061520f090960ed25902ab
liberbell <liberbell@gmail.com>
2019-01-22 08:01:32 +0900

    image_recognition_pretraing7

--- image_recognition_pretraing.py
+++ image_recognition_pretraing.py
@@ -7,13 +7,10 @@ model = vgg16.VGG16()
 img = image.load_img('bay.jpg', target_size=(224, 224))
 
 x = image.img_to_array(img)
-
 x = np.expand_dims(x, axis=0)
-
 x = vgg16.preprocess_input(x)
 
 predictions = model.predict(x)
-
 predected_clases = vgg16.decode_predictions(predictions, top=9)
 
 print('Top predictions for thsi images:')
