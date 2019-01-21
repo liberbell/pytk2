b8034052cfdc86a7b1ddfe505cc28195ea7f70a0
liberbell <liberbell@gmail.com>
2019-01-22 07:42:11 +0900

    making_predictions3

--- making_predictions.py
+++ making_predictions.py
@@ -23,7 +23,7 @@ model = model_from_json(model_structure)
 model.load_weights('model_weights.h5')
 
 img = image.load_img('cat.png', target_size=(32, 32))
-image_to_test = image.image_to_array(img)
+image_to_test = image.img_to_array(img)
 
 list_of_images = np.expand_dims(image_to_test, axis=0)
 
