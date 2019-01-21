e3be32de4952c3550aed78cf4082caeac14b59a8
liberbell <liberbell@gmail.com>
2019-01-22 07:54:39 +0900

    image_recognition_pretraing3

--- image_recognition_pretraing.py
+++ image_recognition_pretraing.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras.preprocessing import image
 from keras.applications import vgg16
 
-model =
+model = vgg16.VGG16()
 
 img = image.load_img('bay.jpg', target_size=(224, 224))
 
@@ -11,3 +11,9 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 
 x =
+
+predictions =
+
+predected_clases =
+
+print('Top predictions for thsi images:')
