5677a46028741d04583bd7dbb510317f4efc5113
liberbell <liberbell@gmail.com>
2019-01-22 07:52:27 +0900

    image_recognition_pretraing3

--- image_recognition_pretraing.py
+++ image_recognition_pretraing.py
@@ -2,4 +2,12 @@ import numpy as np
 from keras.preprocessing import image
 from keras.applications import vgg16
 
-model = 
+model =
+
+img = image.load_img('bay.jpg', target_size=(224, 224))
+
+x = image.img_to_array(img)
+
+x = np.expand_dims(x, axis=0)
+
+x =
