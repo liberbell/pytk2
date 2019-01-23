b5f80cef20a83a3870f8fc89f1a5ea2f762cb6ab
liberbell <liberbell@gmail.com>
2019-01-24 07:59:57 +0900

    training_with_extracted_features3

--- training_with_extracted_features.py
+++ training_with_extracted_features.py
@@ -5,13 +5,8 @@ import joblib
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train = x_train / 255
-x_test = x_test /255
-
-y_train = keras.utils.to_categorical(y_train, 10)
-y_test = keras.utils.to_categorical(y_test, 10)
+x_train = joblib.load('x_train.dat')
+y_train = joblib.load('y_train.dat')
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
