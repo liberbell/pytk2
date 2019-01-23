7b0d8f5baa89073a1f6df39a64ea71380758506b
liberbell <liberbell@gmail.com>
2019-01-23 12:55:05 +0900

    feature_extraction2

--- feature_extraction.py
+++ feature_extraction.py
@@ -3,3 +3,7 @@ import numpy as np
 import joblib
 from keras.preprocessing import image
 from keras.applications import vgg16
+
+
+dog_path = Path('training_data') / 'dogs'
+not_dog_path = Path('training_data') / 'not_dogs'
