89c7d8a585a0ff540ff229b15a0696bfe2b84d97
liberbell <liberbell@gmail.com>
2019-01-24 07:36:25 +0900

    feature_extraction1

--- feature_extraction.py
+++ feature_extraction.py
@@ -7,3 +7,14 @@ from keras.applications import vgg16
 
 dog_path = Path('training_data') / 'dogs'
 not_dog_path = Path('training_data') / 'not_dogs'
+
+images = []
+labels = []
+
+for img in not_dog_path.glob('*.ong'):
+    img = image.load_img(mg)
+
+    image_array = image.img_to_array(img)
+    images.append(image_array)
+
+    labels.append(0)
