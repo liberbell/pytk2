1be6d4d37814460755ab9f560922e002f0cb6f39
liberbell <liberbell@gmail.com>
2019-01-27 18:51:17 +0900

    cloud_image_recognition3

--- cloud_image_recognition.py
+++ cloud_image_recognition.py
@@ -5,3 +5,5 @@ from oauth2client.client import GoogleCredentials
 
 IMAGE_FILE = 'road_sign.jpg'
 CREDENTIALS_FILE = 'credential.json'
+
+credentials = GoogleCredentials.from_stream(CREDENTIALS_FILE)
