4c844458824482f4a0897ae98e0ffc776bfb5607
liberbell <liberbell@gmail.com>
2019-01-27 18:54:03 +0900

    cloud_image_recognition4

--- cloud_image_recognition.py
+++ cloud_image_recognition.py
@@ -7,3 +7,8 @@ IMAGE_FILE = 'road_sign.jpg'
 CREDENTIALS_FILE = 'credential.json'
 
 credentials = GoogleCredentials.from_stream(CREDENTIALS_FILE)
+service = googleapiclient.discovery.build('vison', 'v1', credentials=credentials)
+
+with open(IMAGE_FILE, mode='rb') as f:
+    image_data = f.read()
+    encoded_image_data = b64encode(image_data).decode('UTF-8')
