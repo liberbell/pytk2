c1f4a0d3cbe8852e2319ac6d11ba7a779e7f2bdd
liberbell <liberbell@gmail.com>
2019-01-27 18:46:19 +0900

    cloud_image_recognition2

--- cloud_image_recognition.py
+++ cloud_image_recognition.py
@@ -1,3 +1,7 @@
 from base64 import b64encode
 
 import googleapiclient.discovery
+from oauth2client.client import GoogleCredentials
+
+IMAGE_FILE = 'road_sign.jpg'
+CREDENTIALS_FILE = 'credential.json'
